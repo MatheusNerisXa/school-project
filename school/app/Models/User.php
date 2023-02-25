de39c9704da8fc33d3544c08b2cd5dc4b86f131f
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-02-25 20:31:17 -0300

    email verificacao e email reset senha

--- school/app/Models/User.php
+++ school/app/Models/User.php
@@ -3,15 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\ResetPassword;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
     /**
      * The attributes that are mass assignable.
      *
---
+++ school/app/Notifications/ResetPassword.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends Notification
+{
+    use Queueable;
+    private $token;
+
+    /**
+     * Create a new notification instance.
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Recuperação de Senha')
+            ->line('Você está recebendo este e-mail porque recebemos um pedido de redefinição de senha para sua conta.')
+            ->action('Resetar Senha', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('Se você não solicitou uma alteração da senha, nenhuma ação adicional é necessária.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
